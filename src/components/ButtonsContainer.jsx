475ff620b2db5c59406df82c83724467f676651f
pathareshubham98-dot <pathareshubham98@gmail.com>
2026-07-19 11:39:48 +0530

    calculator changed in button & new upgrades

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,13 +1,32 @@
 import styles from "./ButtonsContainer.module.css";
 
 const ButtonsContainer = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+
   return (
-  <div className={styles.buttonsContainer}>
-    <button className={styles.button}>C</button>
-    <button className={styles.button}>1</button>
-    <button className={styles.button}>2</button>
-    <button className={styles.button}>+</button>
-  </div>
+    <div className={styles.buttonsContainer}>
+      {buttonNames.map((buttonName) => (
+        <button className={styles.button}>{buttonName}</button>
+      ))}
+    </div>
   );
 };
 
